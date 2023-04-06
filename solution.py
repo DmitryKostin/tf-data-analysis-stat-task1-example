927c267996c21c3a18f8703b3be1be70fbece43a
Dmitry Kostin <112777722+DmitryKostin@users.noreply.github.com>
2023-04-06 14:33:32 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,9 +5,8 @@ import numpy as np
 chat_id = 944932368
 
 def solution(x: np.array) -> float:
-    n = len(x) 
-    t = 30
-    sample_mean = np.mean(x) 
-    sample_var = np.var(x, ddof=1) 
-    lambda_hat = (sample_var - sample_mean) / (t*(n-1))
+    n = len(x)  
+    t = 30  
+    sample_mean = np.mean(x)  
+    lambda_hat = sample_mean / t
     return lambda_hat
