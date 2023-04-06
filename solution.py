3a3b7b4d8b1cd26da66dbe8c4c31f309198cd394
Dmitry Kostin <112777722+DmitryKostin@users.noreply.github.com>
2023-04-06 13:46:34 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,12 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 944932368
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    n = len(x) 
+    t = 30
+    sample_mean = np.mean(x) 
+    sample_var = np.var(x, ddof=1) 
+    lambda_hat = (sample_var - sample_mean) / (t*(n-1))
+    return lambda_hat
